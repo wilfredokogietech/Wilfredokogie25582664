4cc1f0ea4ab95174b8b5c38d652107fc37c0954b
wilfredokogietech <102659493+wilfredokogietech@users.noreply.github.com>
2022-03-31 15:31:37 +0100

    Add files via upload

---
+++ Wilfred_okogie-master/TextAnalysizer/Analyzer.cs
@@ -0,0 +1,125 @@
+﻿namespace TextAnalyzer
+{
+    public class Analyzer
+    {
+        private readonly Dictionary<char, int> analyzer = new();
+        public Analyzer(string text)
+        {
+            Text = text;
+        }
+        public string Text { get; set; }        
+
+        /// <summary>
+        /// Gets the total number of sentences
+        /// </summary>
+        /// <returns></returns>
+        public int CountSentences()
+        {
+            return Sentences(Text).Length;
+        }
+
+        /// <summary>
+        /// Gets the total number of vowels in the whole texts
+        /// </summary>
+        /// <returns></returns>
+        public int CountVowels()
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+            return CleanText().Count(x => vowels.Contains(x));
+        }
+
+        /// <summary>
+        /// Gets the total number of consonants in the whole texts
+        /// </summary>
+        /// <returns></returns>
+        public int CountConsonant()
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+            return CleanText().Count(x => !vowels.Contains(x));
+        }
+
+        /// <summary>
+        /// Gets the total number of upper case letters
+        /// </summary>
+        /// <returns></returns>
+        public int CountUpperCaseLetters()
+        {
+            return CleanText().Count(x => char.IsUpper(x));
+        }
+
+        /// <summary>
+        /// Gets the total number of lower case letters
+        /// </summary>
+        /// <returns></returns>
+        public int CountLowerCaseLetters()
+        {
+            return CleanText().Count(x => char.IsLower(x));
+        }       
+
+        /// <summary>
+        /// Gets the total number for each letter
+        /// </summary>
+        /// <returns></returns>
+        public Dictionary<char, int> CountIndividualLetters()
+        {
+            var text = CleanText();
+
+            foreach (char character in text)
+            {
+                if (analyzer.ContainsKey(character))
+                {
+                    analyzer[character] += 1;
+                }
+                else
+                {
+                    analyzer[character] = 1;
+                }
+            }
+
+            return analyzer;
+        }
+
+        /// <summary>
+        /// Removes all other characters except letters
+        /// </summary>
+        /// <returns></returns>
+        private string CleanText()
+        {
+            return string.Join("", FlattenSentences().Where(x => char.IsLetter(x)));
+        }
+
+        /// <summary>
+        /// Groups the multi-line sentences into a single string
+        /// </summary>
+        /// <returns></returns>
+        private string FlattenSentences()
+        {
+            var sentences = Text.Split("*").ToArray();
+            var str = string.Join("", sentences);
+
+            return str;
+        }
+
+        public static bool ValidateText(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                return false;
+
+            return true;
+        }
+
+        /// <summary>
+        /// Gets the sentences in a array
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        private static string[] Sentences(string text)
+        {
+            var lines = text.Split("*").Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
+
+            return lines;
+        }
+    }
+}
---
+++ Wilfred_okogie-master/TextAnalysizer/FileReader.cs
@@ -0,0 +1,26 @@
+﻿namespace TextAnalyzer
+{
+    public static class FileReader
+    {
+        /// <summary>
+        /// Reads the text in a file
+        /// </summary>
+        /// <returns></returns>
+        public static string? ReadTextFile()
+        {
+            try
+            {
+                var sr = File.ReadAllLines(@"TextFile.txt");
+                
+                return string.Join("", sr);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+
+                throw;
+            }
+        }
+    }
+}
